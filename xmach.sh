6afb3831fbf4df44ce649f1487c701f0315bbadf
W. Davis <wdavis4@bates.edu>
2018-05-02 18:16:02 +0000

    y

---
+++ xmach.sh
@@ -0,0 +1,22 @@
+
+echo "In order to continue using this computer, you must first answer 3 riddles"
+echo " "
+echo "type -OK- to continue"
+echo " "
+read OK
+echo " "
+echo "If a blue man lives in the blue house, a red man lives in the red house, and green man lives in the green house. Who lives in the white house?"
+echo " "
+read President
+echo " "
+echo " "
+echo "If you have two coins which total 35 cents and one of the coins is not a dime, what are the two coins?"
+read stuff
+echo " "
+echo " "
+echo " "
+echo "What do mathematics teachers like to eat?"
+echo " "
+read pi
+echo " "
+echo "You now have regained full access to this computer"
